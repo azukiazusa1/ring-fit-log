8ee4ac591e43b5b8ae52befa777f1b7aadd23178
azukiazusa1 <cycleippei@gmail.com>
2020-06-23 22:03:36 +0900

    delete console.log

--- middleware/uid.ts
+++ middleware/uid.ts
@@ -2,16 +2,12 @@ import { Middleware } from '@nuxt/types'
 
 const uid: Middleware = async ({ error, $axios, $auth, app }) => {
   const { $cookies } = app
-  console.log($auth.loggedIn)
   const uid: string = $cookies.get('uid')
-  console.log(!uid)
   if ($auth.loggedIn && !uid) {
-    console.log(1)
     try {
       const { data } = await $axios.post('/api/users', {
         user: $auth.user
       })
-      console.log(data)
       $cookies.set('uid', data.uid, {
         maxAge: 60 * 60 * 24 * 7,
         sameSite: 'strict'
