1761427e9981d51de9bc1a9f84052ea19817c8d6
azukiazusa1 <cycleippei@gmail.com>
2020-06-25 22:14:05 +0900

    testing setup

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -5,10 +5,31 @@ import userRoutes from './userRoutes'
 const router = Express.Router()
 
 router.use('/users', userRoutes)
+router.use('/todos', (_req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: 'リスト1',
+      description: 'lorem ipsum',
+      done: true
+    },
+    {
+      id: 2,
+      title: 'リスト2',
+      description: 'lorem ipsum',
+      done: false
+    },
+    {
+      id: 3,
+      title: 'リスト3',
+      description: 'lorem ipsum',
+      done: true
+    }
+  ])
+})
 router.use('/', (req, res) => {
-  const cookies = new Cookies(req.headers.cookie)
-  console.log(cookies)
-  res.json({ status: 'ok' })
+  res.status(404)
+  res.json({ message: 'Not Found' })
 })
 
 export default router
