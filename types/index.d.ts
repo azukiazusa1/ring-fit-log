8dc29def90c125502db5d58fae0f5406938aa3bf
azukiazusa1 <cycleippei@gmail.com>
2020-06-26 18:37:21 +0900

    snackbar type

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,13 +2,18 @@ export interface Greeting {
   greeting: string
 }
 
+export type Types = 'info' | 'error' | 'worning' | 'success'
+
 export interface SnackbarPayload {
   message: string
-  color?: string
   escape?: boolean
   top?: boolean
   right?: boolean
   left?: boolean
   bottom?: boolean
   timeout?: number
+}
+
+export interface SnacbarTypePayload extends SnackbarPayload {
+  type: Types
 }
