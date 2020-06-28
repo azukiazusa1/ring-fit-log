ecc8817a4e825df053d73ddc58a69518f6d3a348
azukiazusa1 <cycleippei@gmail.com>
2020-06-28 23:25:02 +0900

    submit button

---
+++ components/SubmitButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn rounded color="primary" min-width="300" @click="click">
+    <slot />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'SubmitButton',
+
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
+fa
