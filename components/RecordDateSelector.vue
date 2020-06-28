199445a8aa0a176c6a19502f3ffc6abfe29fd0f3
azukiazusa1 <cycleippei@gmail.com>
2020-06-28 19:50:34 +0900

    日付操作

---
+++ components/RecordDateSelector.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-row>
+    <v-col md="2" class="d-none d-md-flex">
+      <v-btn :to="`/record?date=${prevWeek}`" icon>
+        <v-icon>fas fa-angle-double-left</v-icon>
+      </v-btn>
+    </v-col>
+    <v-col cols="3" md="2">
+      <v-btn :to="`/record?date=${prevDay}`" icon>
+        <v-icon>fas fa-angle-left</v-icon>
+      </v-btn>
+    </v-col>
+    <v-col cols="6" md="4">
+      <h2 class="headline text-center">{{ formatedDate }}</h2>
+    </v-col>
+    <v-col cols="1" md="1" offset="1">
+      <v-btn :to="`/record?date=${nextDay}`" icon>
+        <v-icon>fas fa-angle-right</v-icon>
+      </v-btn>
+    </v-col>
+    <v-col md="1" offset="1" class="d-none d-md-flex">
+      <v-btn :to="`/record?date=${nextWeek}`" icon>
+        <v-icon>fas fa-angle-double-right</v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'RecordDateSelector',
+  props: {
+    date: {
+      type: Date,
+      required: true
+    }
+  },
+  computed: {
+    nextDay(): string {
+      return this.$moment(this.date)
+        .add(1, 'days')
+        .format('YYYY-MM-DD')
+    },
+    prevDay(): string {
+      return this.$moment(this.date)
+        .add(-1, 'days')
+        .format('YYYY-MM-DD')
+    },
+    nextWeek(): string {
+      return this.$moment(this.date)
+        .add(1, 'weeks')
+        .format('YYYY-MM-DD')
+    },
+    prevWeek(): string {
+      return this.$moment(this.date)
+        .add(-1, 'weeks')
+        .format('YYYY-MM-DD')
+    },
+    formatedDate(): string {
+      return this.$moment(this.date).format('YYYY/MM/DD（ddd）')
+    }
+  }
+})
+</script>
