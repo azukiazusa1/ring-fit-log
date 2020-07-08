d26a93b5b975ecdf7dc2bc1d80d28cb801a52d0c
azukiazusa1 <cycleippei@gmail.com>
2020-07-08 23:06:16 +0900

    time picker

--- components/atom/TotalTimeExercisingInputField.vue
+++ components/atom/TotalTimeExercisingInputField.vue
@@ -1,21 +1,33 @@
 <template>
-  <v-text-field
+  <date-picker
     v-model="_totalTimeExercising"
-    :error-messages="totalTimeExercisingErrors"
-    prepend-icon="fas fa-stopwatch"
+    :clearable="false"
     type="time"
-    step="1"
-    outlined
-    dense
-    color="orange darken-1"
-    label="活動時間"
-    @input="$v._totalTimeExercising.$touch()"
-    @blur="$v._totalTimeExercising.$touch()"
-  ></v-text-field>
+    format="HH:mm:ss"
+    value-type="HH:mm:ss"
+  >
+    <template #input>
+      <v-text-field
+        :value="_totalTimeExercising"
+        prepend-icon="fas fa-stopwatch"
+        outlined
+        clearable
+        dense
+        readonly
+        color="orange darken-1"
+        label="活動時間"
+        @input="$v._totalTimeExercising.$touch()"
+        @blur="$v._totalTimeExercising.$touch()"
+      />
+    </template>
+    <template #icon-calendar> </template>
+  </date-picker>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/index.css'
 import { validationMixin } from 'vuelidate'
 import { helpers } from 'vuelidate/lib/validators'
 import { timeError } from '~/config/validationErrorMessages'
@@ -23,6 +35,9 @@ const time = helpers.regex('time', /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/)
 
 export default Vue.extend({
   name: 'TotalTimeExercisingInputField',
+  components: {
+    DatePicker
+  },
   mixins: [validationMixin],
   validations: {
     _totalTimeExercising: {
@@ -62,6 +77,27 @@ export default Vue.extend({
     validate(value) {
       if (value) this.$v.$touch()
     }
+  },
+  methods: {
+    onBlur() {
+      console.log('onBlur')
+      this.$v._totalTimeExercising.$touch()
+    }
   }
 })
 </script>
+
+<style>
+.mx-datepicker {
+  width: 100%;
+}
+
+.mx-icon-calendar {
+  display: none;
+}
+
+.mx-datepicker-body {
+  position: relative;
+  bottom: 25px;
+}
+</style>
---
+++ types/vue-ctk-date-time-picker.d.ts
@@ -0,0 +1,4 @@
+declare module 'vue-ctk-date-time-picker' {
+  import Vue from 'vue'
+  export default Vue
+}
