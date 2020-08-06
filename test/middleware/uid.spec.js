bbc2427f6fbd6eea05a2647786ce8e97f06b4ed5
azukiazusa1 <cycleippei@gmail.com>
2020-08-06 21:57:39 +0900

    uid midddleware test

--- test/middleware/uid.spec.js
+++ test/middleware/uid.spec.js
@@ -9,21 +9,22 @@ const $axios = {
     if (mockError) {
       return Promise.reject('mock error')
     }
-    return Promise.resolve({ uid })
+    return Promise.resolve({ data: { uid } })
   })
 }
 
 const cb = () => {}
+const loginUser = {
+  username: 'username',
+  strategy: 'github',
+  identifier: 'abcde',
+  email: 'aaa@example.com',
+  photoURL: 'example'
+}
 
 jest.mock('~/utils/getLoginUser')
 getLoginUser.mockImplementation(() => {
-  return {
-    username: 'username',
-    strategy: 'github',
-    identifier: 'abcde',
-    email: 'aaa@example.com',
-    photoURL: 'example'
-  }
+  return loginUser
 })
 
 describe('~/middleware/uid', () => {
@@ -40,18 +41,87 @@ describe('~/middleware/uid', () => {
           set: jest.fn()
         }
       }
-      const $auth = {
-        loggedIn: false
-      }
-      const context = {
-        error,
-        $axios,
-        app,
-        $auth
-      }
+      const $auth = { loggedIn: false }
+      const context = { error, $axios, app, $auth }
       test('$axios.postが呼ばれない', async () => {
         await middleware(context, cb)
         expect($axios.post).toHaveBeenCalledTimes(0)
+      })
+    })
+    describe('未ログイン && cookieにuidがある', () => {
+      const app = {
+        $cookies: {
+          get() {
+            return uid
+          },
+          set: jest.fn()
+        }
+      }
+      const $auth = { loggedIn: false }
+      const context = { error, $axios, app, $auth }
+      test('$axios.postが呼ばれない', async () => {
+        await middleware(context, cb)
+        expect($axios.post).toHaveBeenCalledTimes(0)
+      })
+    })
+    describe('ログイン済 && cookieにuidがある', () => {
+      const app = {
+        $cookies: {
+          get() {
+            return uid
+          },
+          set: jest.fn()
+        }
+      }
+      const $auth = { loggedIn: true }
+      const context = { error, $axios, app, $auth }
+      test('$axios.postが呼ばれない', async () => {
+        await middleware(context, cb)
+        expect($axios.post).toHaveBeenCalledTimes(0)
+      })
+    })
+
+    describe('ログイン済 && cookieにuidがない', () => {
+      const app = {
+        $cookies: {
+          get() {
+            return ''
+          },
+          set: jest.fn()
+        }
+      }
+      const $auth = { loggedIn: true }
+      const context = { error, $axios, app, $auth }
+      test('$axios.postが正しいurlとデータで呼ばれる', async () => {
+        await middleware(context, cb)
+        expect($axios.post.mock.calls[0][0]).toEqual('/api/users')
+        expect($axios.post.mock.calls[0][1]).toEqual({ user: loginUser })
+      })
+
+      test('cookieに返却されたuidがセットされる', async () => {
+        await middleware(context, cb)
+        expect(app.$cookies.set.mock.calls[0][0]).toEqual('userId')
+        expect(app.$cookies.set.mock.calls[0][1]).toEqual(uid)
+      })
+    })
+  })
+
+  describe('異常系', () => {
+    describe('$axiosでエラーが発生したとき', () => {
+      test('error関数が呼ばれる', async () => {
+        mockError = true
+        const app = {
+          $cookies: {
+            get() {
+              return ''
+            },
+            set: jest.fn()
+          }
+        }
+        const $auth = { loggedIn: true }
+        const context = { error, $axios, app, $auth }
+        await middleware(context, cb)
+        expect(error).toHaveBeenCalled()
       })
     })
   })
