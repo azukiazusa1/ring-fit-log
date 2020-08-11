da6cd3a01081c0ac912a4d3551f6494a1329fc44
azukiazusa1 <cycleippei@gmail.com>
2020-08-11 21:46:33 +0900

    Chart class

--- api/controllers/chartsController.ts
+++ api/controllers/chartsController.ts
@@ -1,9 +1,29 @@
 import Express from 'express'
 import Chart from '../models/Chart'
+import Boom from '@hapi/boom'
+import httpStatusCode from 'http-status-codes'
+import isInvalidDate from '../../utils/isInvalidDate'
 
 export default {
-  week: (_req: Express.Request, res: Express.Response) => {
-    res.status(200).json(Chart.month)
+  week: async (
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) => {
+    if (isInvalidDate(req.params.date)) {
+      next(Boom.badRequest())
+    }
+
+    const date = new Date(req.params.date)
+    const userId: string = res.locals.userId
+    const chart = new Chart(date, userId)
+
+    try {
+      const chartData = await chart.week()
+      res.status(httpStatusCode.OK).json(chartData)
+    } catch (e) {
+      next(Boom.internal())
+    }
   },
   quarter: (_req: Express.Request, res: Express.Response) => {
     res.json(Chart.quarter)
