e59eb5c7c76f67fe94d865f38eb746347fc9d968
azukiazusa1 <cycleippei@gmail.com>
2021-01-09 18:33:45 +0900

    ｓ

--- pages/aggregation.vue
+++ pages/aggregation.vue
@@ -6,7 +6,22 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { AggregateStore, SettingStore, SnackbarModule } from '~/store'
 
 export default Vue.extend({
+  async asyncData({ app, query, $moment }): Promise<Data> {
+    try {
+      await AggregateStore.fetch()
+    } catch (e) {
+      SnackbarModule.error({
+        message: 'データの取得に失敗しました。'
+      })
+    }
+  },
+  head() {
+    return {
+      title: '集計'
+    }
+  },
 })
 </script>
