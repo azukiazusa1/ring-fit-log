9ea9c2ff1771712d181032160ecbee08b99eb3db
azukiazusa1 <cycleippei@gmail.com>
2021-06-06 09:46:19 +0900

    close #50

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,18 +1,41 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div class="wrapper">
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-img
+          :src="require('~/assets/image/error-cat.jpg')"
+          alt="error-cat"
+          contain
+          max-height="500"
+        />
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="pa-2">
+          <v-card-title class="text-h1 mb-5">
+            {{ error.statusCode }}
+          </v-card-title>
+          <v-card-text>
+            <p v-if="error.statusCode === 404">
+              {{ pageNotFound }}
+            </p>
+            <p v-else>
+              {{ otherError }}
+            </p>
+          </v-card-text>
+          <v-card-actions class="justify-center">
+            <v-btn color="primary" @click="showReportDialog">
+              エラーを報告する
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import * as Sentry from '@sentry/vue'
+import getLoginUser from '~/utils/getLoginUser'
 export default {
   layout({ $auth }) {
     return $auth.loggedIn ? 'default' : 'unauthorized'
@@ -25,12 +48,33 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound:
+        'お探しのページが見つかりませんでした。移動もしくは削除された可能性があります。 また、URL、ファイル名にタイプミスがないか再度ご確認ください。',
+      otherError:
+        '申し訳ありません。予期せぬエラーが発生しました。エラーを報告していただけることを、心より感謝しております。',
+      eventId: ''
+    }
+  },
+  computed: {
+    user() {
+      if (!this.$auth.loggedIn) return null
+      return this.$cookies.get('userInfo') || getLoginUser(this.$auth)
     }
   },
   created() {
-    this.$sentry.captureException(this.error)
+    this.eventId = this.$sentry.captureException(this.error)
+  },
+  methods: {
+    showReportDialog() {
+      Sentry.showReportDialog({
+        eventId: this.eventId,
+        dsn: process.env.SENTRY_DSN,
+        user: {
+          name: this.user ? this.user.username : '',
+          email: this.user ? this.user.email : ''
+        }
+      })
+    }
   },
   head() {
     const title =
@@ -43,7 +87,8 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+.wrapper {
+  max-width: 980px;
+  margin: 0 auto;
 }
 </style>
