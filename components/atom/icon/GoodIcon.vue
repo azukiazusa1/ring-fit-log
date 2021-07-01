822054cbe2cd3769ebb9c48ffb9d7a9ed17a7be6
azukiazusa1 <cycleippei@gmail.com>
2021-07-01 23:35:36 +0900

    good icon

---
+++ components/atom/icon/GoodIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn :color="color" icon @click="click">
+    <v-icon>fas fa-thumbs-up</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'GoodIcon',
+  props: {
+    value: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    color(): string {
+      return this.value ? 'orange darken-1' : ''
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
