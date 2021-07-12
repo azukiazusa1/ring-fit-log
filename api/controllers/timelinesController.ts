e71f4242bbb84ebcd3c52801fe486dbaeb30ef99
azukiazusa1 <cycleippei@gmail.com>
2021-07-12 23:03:59 +0900

    timeline controller list action

--- api/controllers/timelinesController.ts
+++ api/controllers/timelinesController.ts
@@ -1,17 +1,27 @@
 import Express from 'express'
 import httpStatus from 'http-status-codes'
 import Boom from '@hapi/boom'
+import Timeline from '../models/Timeline'
 
 export default {
-  list: (
+  list: async (
     req: Express.Request,
     res: Express.Response,
     next: Express.NextFunction
   ) => {
-    const { page, limit } = req.body
+    const { page, limit } = req.query
     try {
-      res.status(httpStatus.OK).json({ message: 'OK' })
+      const result = await Timeline.paginate(
+        {},
+        {
+          page: Number(page),
+          limit: Number(limit),
+          sort: { createdAt: -1 }
+        }
+      )
+      res.status(httpStatus.OK).json(result)
     } catch (e) {
+      console.log(e)
       next(Boom.internal())
     }
   }
--- api/models/Timeline.ts
+++ api/models/Timeline.ts
@@ -1,4 +1,14 @@
-import mongoose, { Schema, Document, Model, models } from 'mongoose'
+import mongoose, {
+  Schema,
+  Document,
+  Model,
+  models,
+  FilterQuery,
+  PaginateOptions,
+  PaginateResult
+} from 'mongoose'
+import mongoosePaginate from 'mongoose-paginate-v2'
+import mongooseAutoPopulate from 'mongoose-autopopulate'
 import { LoginUser } from '~/types/auth'
 import { Record } from '~/types/record'
 
@@ -13,12 +23,24 @@ const timelineSchema: Schema = new Schema(
     user: {
       type: Schema.Types.ObjectId,
       ref: 'User',
-      required: true
+      required: true,
+      autopopulate: {
+        select: ['username', 'photoURL']
+      }
     },
     record: {
       type: Schema.Types.ObjectId,
       ref: 'Record',
-      required: true
+      required: true,
+      autopopulate: {
+        select: [
+          'stamps',
+          'comment',
+          'totalDistanceRun',
+          'totalCaloriesBurned',
+          'totalTimeExercising'
+        ]
+      }
     },
     likes: {
       type: [String],
@@ -29,7 +51,16 @@ const timelineSchema: Schema = new Schema(
     timestamps: true
   }
 )
-interface TimelineModel extends Model<TimelineDoc> {}
+interface TimelineModel extends Model<TimelineDoc> {
+  paginate(
+    query?: FilterQuery<TimelineDoc>,
+    options?: PaginateOptions,
+    callback?: (err: any, result: PaginateResult<TimelineDoc>) => void
+  ): Promise<PaginateResult<TimelineDoc>>
+}
+
+timelineSchema.plugin(mongoosePaginate)
+timelineSchema.plugin(mongooseAutoPopulate)
 
 export default models.Timeline
   ? (models.Timeline as TimelineModel)
