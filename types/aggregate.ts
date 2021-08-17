06ac8f7ce4face2d4ebb91ee6f74a43174df0c37
azukiazusa1 <cycleippei@gmail.com>
2021-08-17 23:29:11 +0900

    aggregate type

--- types/aggregate.ts
+++ types/aggregate.ts
@@ -2,6 +2,12 @@ export interface AverageData {
   avgTimeExercising: number
   avgCaloriesBurned: number
   avgDistanceRun: number
+  maxTimeExercising: number
+  maxCaloriesBurned: number
+  maxDistanceRun: number
+  sumTimeExercising: number
+  sumCaloriesBurned: number
+  sumDistanceRun: number
 }
 
 export interface FrequentTime {
